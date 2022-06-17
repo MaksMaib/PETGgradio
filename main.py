42d77ef21ae0281ec30e448f445e2c349088ce7b
maksmaib <max222may@gmail.com>
2022-06-17 17:28:29 +0300

    change all to heroku

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import gradio as gr
-import model
+
+import cv2
 
 
-def inference(content, style):
-    result = model.main(style.name, content.name)
-    return result
+def inference(image):
+
+   img = cv2.imread(image.name)
+   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+   return gray
 
 
 if __name__ == "__main__":
     title = " Part of my PET project"
     description = "One of my attempts to combine machine learning models and a web interface. Gredio as black magic prototype for visualization of the model is ready in a couple of hours."
     git = "<p style='text-align: center'> <a href='https://github.com/MaksMaib/PETGgradio' target='_blank'>Github Repo</a></p>"
-    examples = [
-        ['picasso-old.jpg', 'picasso.jpg']
-        ]
 
-    inputs = [gr.inputs.Image(type="file", label='content'), gr.inputs.Image(type="file", label='style')]
+
+    inputs = [gr.inputs.Image(type="file", label='image')]
     output = gr.outputs.Image(type="pil")
     iface = gr.Interface(inference, inputs, outputs=output, title=title, article=git,
-                         description=description, examples=examples)
+                         description=description)
     iface.launch()
